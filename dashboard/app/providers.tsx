03995729b2aa884fe2ea4c361e6678565fbb2c17
Abheek Tripathy <abheek.tripathy@gmail.com>
2025-02-06 12:14:23 +0530

    remove comment

--- dashboard/app/providers.tsx
+++ dashboard/app/providers.tsx
@@ -12,7 +12,6 @@ import { mainnet } from 'viem/chains';
 
 const queryClient = new QueryClient();
 
-// THIS IS NOT NEEDED FOR LENS
 export const config = getDefaultConfig({
   appName: 'TurboDa',
   projectId: 'ff3e7e095aae4b0550ed934c1539ed07',
