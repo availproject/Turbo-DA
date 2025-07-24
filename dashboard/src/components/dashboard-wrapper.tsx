f2ea28fd572002ea410497fe0d9d545d71f750b2
dakshi goel <dakshiegoel@gmail.com>
2025-07-24 18:05:43 +0530

    added api endpoint

--- dashboard/src/components/dashboard-wrapper.tsx
+++ dashboard/src/components/dashboard-wrapper.tsx
@@ -4,6 +4,7 @@ import { useOverview } from "@/providers/OverviewProvider";
 import { memo, ReactNode } from "react";
 import { Tabs, TabsList, TabsTrigger } from "./tabs";
 import { Text } from "./text";
+import { Copy } from "lucide-react";
 
 const DashboardWrapper = ({ children }: { children: ReactNode }) => {
   const { setMainTabSelected, mainTabSelected } = useOverview();
@@ -32,6 +33,7 @@ const DashboardWrapper = ({ children }: { children: ReactNode }) => {
               Buy credits, post data at a guaranteed rate.
             </Text>
             <Tabs value={mainTabSelected} className="w-full gap-y-0 mt-3">
+              <div className="flex justify-between">
               <TabsList className="bg-transparent p-0 h-auto relative -bottom-px">
                 {mainTabs.map((tab) => (
                   <TabsTrigger
@@ -43,6 +45,28 @@ const DashboardWrapper = ({ children }: { children: ReactNode }) => {
                   </TabsTrigger>
                 ))}
               </TabsList>
+             <div
+  className="w-[464px] px-[8px] py-[12px] border border-[#4C4C4C] rounded-t-[8px] flex items-center"
+  style={{
+    background: "var(--Gradient-Secondary-Base, linear-gradient(90deg, var(--Color-bg-gradients-surface-secondary-leftshade, #0D2335) 0%, var(--Color-bg-gradients-surface-secondary-rightshade, #141B29) 100%))",
+  }}
+>
+
+  <p className="text-[#CCC] font-semibold leading-[18px] text-sm">API Endpoint: <span className="text-[#3CA3FC] text-base">https://infinity.turbo-api.availproject.org/ </span>
+  
+  </p>
+  <Copy 
+    className="ml-[6px] cursor-pointer hover:opacity-80"
+    size={20}
+    color="#B3B3B3"
+    strokeWidth={2}
+    onClick={() => {
+      navigator.clipboard.writeText('https://infinity.turbo-api.availproject.org/');
+    }}
+  />
+</div>
+
+              </div>
               {children}
             </Tabs>
           </div>
