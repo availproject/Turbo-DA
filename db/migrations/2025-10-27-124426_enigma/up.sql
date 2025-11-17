20364b687e048dfd53d8dcab14eeb5fe99208171
Rachit Srivastava <rac.sri25@gmail.com>
2025-11-17 14:59:24 +0530

    Develop (#107)
    * feat: configure country based geoblocking
    
    * quick fix for command.tsx
    
    * build fix
    
    * add store
    
    * update country blocking
    
    * copy changes
    
    * feat: enigma da
    
    * feat: fallback fix
    
    * feat: refactoring'
    
    * feat: data submission
    
    * feat: make cert accesible via env
    
    * add encryption toggle
    
    * feat: logs
    
    * disable engima + edit geoblock country list
    
    * feat: fix migration
    
    * feat: spam filter
    
    ---------
    
    Co-authored-by: Abheek Tripathy <abheek.tripathy@gmail.com>
    Co-authored-by: 0xAbhk <90976669+abheektripathy@users.noreply.github.com>

--- db/migrations/2025-10-27-124426_enigma/up.sql
+++ db/migrations/2025-10-27-124426_enigma/up.sql
@@ -7,3 +7,11 @@ SET encryption = FALSE;
 
 ALTER TABLE apps
 ALTER COLUMN encryption SET NOT NULL;
+
+ALTER TABLE customer_expenditures
+ADD COLUMN ciphertext_hash BYTEA,
+ADD COLUMN plaintext_hash BYTEA,
+ADD COLUMN signature_ciphertext_hash BYTEA,
+ADD COLUMN signature_plaintext_hash BYTEA,
+ADD COLUMN address BYTEA,
+ADD COLUMN ephemeral_pub_key BYTEA;
